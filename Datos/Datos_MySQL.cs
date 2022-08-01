7bd02af9de722faf26ac1b8af759f5e57c9a05d1
Michael Vargas S <michaelvargas47@hotmail.com>
2022-08-01 12:14:03 -0600

    Se agrega clase Datos_MYSQL con el string de conexion usando el app.config

---
+++ Datos/Datos_MySQL.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Datos_MySQL
+    {
+        MySqlConnection mySQLconector = new MySqlConnection();
+        public Datos_MySQL() //constructor
+        {
+            try
+            {
+                StringBuilder StrConexionArmado = new StringBuilder();
+
+                StrConexionArmado.Append("Server=");
+                StrConexionArmado.Append(ConfigurationManager.AppSettings["ServerName"]);
+                StrConexionArmado.Append("; port=");
+                StrConexionArmado.Append(ConfigurationManager.AppSettings["Port"]);
+                StrConexionArmado.Append("; User id=");
+                StrConexionArmado.Append(ConfigurationManager.AppSettings["User"]);
+                StrConexionArmado.Append("; Password=");
+                StrConexionArmado.Append(ConfigurationManager.AppSettings["Password"]);
+
+                this.mySQLconector = new MySqlConnection(StrConexionArmado.ToString());
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }//fin costructor
+
+        /// <summary>
+        /// Funcion que realiza la consulta a una tabla especifica
+        /// </summary>
+        /// <param name="QueryDelUsuario">Trae la consulta en un String desde interfaz</param>
+        /// <returns></returns>
+        public DataTable QueryMySQL_DT(String QueryDelUsuario)
+        {
+            try
+            {
+                MySqlCommand cmd = mySQLconector.CreateCommand();
+ 
+                cmd.CommandText = QueryDelUsuario;
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dtdatos = new DataTable();
+
+                mySQLconector.Open();
+
+                adapter.Fill(dtdatos);
+
+                
+
+                return dtdatos;
+            }
+            catch (MySqlException ex)
+            {
+             throw new Exception(ex.Message);
+            }
+            finally
+            {
+                mySQLconector.Close();
+            }
+        }//fin query
+  
+
+    }//fin class
+}//fin namespace
