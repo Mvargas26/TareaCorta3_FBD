afab94d9345da35f68b58b60e4bb42fc3e80758d
Michael Vargas S <michaelvargas47@hotmail.com>
2022-08-03 01:15:01 -0600

    Pantallas de Loguin y Crear usuario de Andres

---
+++ Interfaz/frmCrearUsuario.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Interfaz
+{
+    partial class frmCrearUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregarU = new System.Windows.Forms.Button();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.lblNUsu = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAgregarU
+            // 
+            this.btnAgregarU.Location = new System.Drawing.Point(158, 304);
+            this.btnAgregarU.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAgregarU.Name = "btnAgregarU";
+            this.btnAgregarU.Size = new System.Drawing.Size(111, 50);
+            this.btnAgregarU.TabIndex = 26;
+            this.btnAgregarU.Text = "Crear Usuario";
+            this.btnAgregarU.UseVisualStyleBackColor = true;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(158, 227);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(281, 22);
+            this.txtContrasena.TabIndex = 25;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(158, 159);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(281, 22);
+            this.txtUsuario.TabIndex = 24;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(158, 90);
+            this.txtApellidos.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(281, 22);
+            this.txtApellidos.TabIndex = 23;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(158, 30);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(281, 22);
+            this.txtNombre.TabIndex = 22;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Location = new System.Drawing.Point(30, 235);
+            this.lblContrasena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(81, 17);
+            this.lblContrasena.TabIndex = 21;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // lblNUsu
+            // 
+            this.lblNUsu.AutoSize = true;
+            this.lblNUsu.Location = new System.Drawing.Point(30, 159);
+            this.lblNUsu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNUsu.Name = "lblNUsu";
+            this.lblNUsu.Size = new System.Drawing.Size(57, 17);
+            this.lblNUsu.TabIndex = 20;
+            this.lblNUsu.Text = "Usuario";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(30, 99);
+            this.lblApellido.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(65, 17);
+            this.lblApellido.TabIndex = 19;
+            this.lblApellido.Text = "Apellidos";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(30, 33);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
+            this.lblNombre.TabIndex = 18;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // frmCrearUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 396);
+            this.Controls.Add(this.btnAgregarU);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.lblNUsu);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "frmCrearUsuario";
+            this.Text = "frmCrearUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregarU;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.Label lblNUsu;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+    }
+}
---
+++ Interfaz/frmLoguin.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Interfaz
+{
+    partial class frmLoguin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LlblCrearUsu = new System.Windows.Forms.LinkLabel();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.lblUsu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LlblCrearUsu
+            // 
+            this.LlblCrearUsu.AutoSize = true;
+            this.LlblCrearUsu.Location = new System.Drawing.Point(173, 267);
+            this.LlblCrearUsu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LlblCrearUsu.Name = "LlblCrearUsu";
+            this.LlblCrearUsu.Size = new System.Drawing.Size(141, 17);
+            this.LlblCrearUsu.TabIndex = 17;
+            this.LlblCrearUsu.TabStop = true;
+            this.LlblCrearUsu.Text = "Crear Nuevo Usuario";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(177, 196);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(108, 41);
+            this.btnIngresar.TabIndex = 16;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(177, 127);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(256, 22);
+            this.txtContrasena.TabIndex = 15;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(177, 34);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(4);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(256, 22);
+            this.txtusuario.TabIndex = 14;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Location = new System.Drawing.Point(48, 136);
+            this.lblContrasena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(81, 17);
+            this.lblContrasena.TabIndex = 13;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // lblUsu
+            // 
+            this.lblUsu.AutoSize = true;
+            this.lblUsu.Location = new System.Drawing.Point(48, 42);
+            this.lblUsu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsu.Name = "lblUsu";
+            this.lblUsu.Size = new System.Drawing.Size(57, 17);
+            this.lblUsu.TabIndex = 12;
+            this.lblUsu.Text = "Usuario";
+            // 
+            // frmLoguin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 322);
+            this.Controls.Add(this.LlblCrearUsu);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.lblUsu);
+            this.Name = "frmLoguin";
+            this.Text = "frmLoguin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel LlblCrearUsu;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.Label lblUsu;
+    }
+}
