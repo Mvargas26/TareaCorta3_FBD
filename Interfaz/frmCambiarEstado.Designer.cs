2c0ac10dace620d41e4ded2ce38e14d4dca1a503
Michael Vargas S <michaelvargas47@hotmail.com>
2022-08-05 14:58:37 -0600

    Cambia el estado correctamente

--- Interfaz/frmCambiarEstado.Designer.cs
+++ Interfaz/frmCambiarEstado.Designer.cs
@@ -29,12 +29,140 @@ namespace Interfaz
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpCambioDeEstado = new System.Windows.Forms.GroupBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardarEstado = new System.Windows.Forms.Button();
+            this.cmbNuevoEstado = new System.Windows.Forms.ComboBox();
+            this.lblNUevoEstado = new System.Windows.Forms.Label();
+            this.cmblistaUsuarios = new System.Windows.Forms.ComboBox();
+            this.lblSelectUsuario = new System.Windows.Forms.Label();
+            this.txtUserModif = new System.Windows.Forms.TextBox();
+            this.lblIDUsuarioModi = new System.Windows.Forms.Label();
+            this.grpCambioDeEstado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpCambioDeEstado
+            // 
+            this.grpCambioDeEstado.Controls.Add(this.txtUserModif);
+            this.grpCambioDeEstado.Controls.Add(this.lblIDUsuarioModi);
+            this.grpCambioDeEstado.Controls.Add(this.btnCancelar);
+            this.grpCambioDeEstado.Controls.Add(this.btnGuardarEstado);
+            this.grpCambioDeEstado.Controls.Add(this.cmbNuevoEstado);
+            this.grpCambioDeEstado.Controls.Add(this.lblNUevoEstado);
+            this.grpCambioDeEstado.Controls.Add(this.cmblistaUsuarios);
+            this.grpCambioDeEstado.Controls.Add(this.lblSelectUsuario);
+            this.grpCambioDeEstado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpCambioDeEstado.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpCambioDeEstado.Location = new System.Drawing.Point(0, 0);
+            this.grpCambioDeEstado.Name = "grpCambioDeEstado";
+            this.grpCambioDeEstado.Size = new System.Drawing.Size(674, 370);
+            this.grpCambioDeEstado.TabIndex = 2;
+            this.grpCambioDeEstado.TabStop = false;
+            this.grpCambioDeEstado.Text = "Cambio de Estado";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(378, 255);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(224, 53);
+            this.btnCancelar.TabIndex = 14;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardarEstado
+            // 
+            this.btnGuardarEstado.Location = new System.Drawing.Point(97, 245);
+            this.btnGuardarEstado.Name = "btnGuardarEstado";
+            this.btnGuardarEstado.Size = new System.Drawing.Size(174, 63);
+            this.btnGuardarEstado.TabIndex = 4;
+            this.btnGuardarEstado.Text = "Guardar Estado";
+            this.btnGuardarEstado.UseVisualStyleBackColor = true;
+            this.btnGuardarEstado.Click += new System.EventHandler(this.btnGuardarEstado_Click);
+            // 
+            // cmbNuevoEstado
+            // 
+            this.cmbNuevoEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNuevoEstado.FormattingEnabled = true;
+            this.cmbNuevoEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.cmbNuevoEstado.Location = new System.Drawing.Point(345, 153);
+            this.cmbNuevoEstado.Name = "cmbNuevoEstado";
+            this.cmbNuevoEstado.Size = new System.Drawing.Size(276, 29);
+            this.cmbNuevoEstado.TabIndex = 3;
+            // 
+            // lblNUevoEstado
+            // 
+            this.lblNUevoEstado.AutoSize = true;
+            this.lblNUevoEstado.Location = new System.Drawing.Point(15, 153);
+            this.lblNUevoEstado.Name = "lblNUevoEstado";
+            this.lblNUevoEstado.Size = new System.Drawing.Size(146, 22);
+            this.lblNUevoEstado.TabIndex = 2;
+            this.lblNUevoEstado.Text = "Nuevo Estado:";
+            // 
+            // cmblistaUsuarios
+            // 
+            this.cmblistaUsuarios.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmblistaUsuarios.FormattingEnabled = true;
+            this.cmblistaUsuarios.Location = new System.Drawing.Point(345, 38);
+            this.cmblistaUsuarios.Name = "cmblistaUsuarios";
+            this.cmblistaUsuarios.Size = new System.Drawing.Size(276, 29);
+            this.cmblistaUsuarios.TabIndex = 1;
+            this.cmblistaUsuarios.SelectedIndexChanged += new System.EventHandler(this.cmblistaUsuarios_SelectedIndexChanged);
+            // 
+            // lblSelectUsuario
+            // 
+            this.lblSelectUsuario.AutoSize = true;
+            this.lblSelectUsuario.Location = new System.Drawing.Point(15, 41);
+            this.lblSelectUsuario.Name = "lblSelectUsuario";
+            this.lblSelectUsuario.Size = new System.Drawing.Size(324, 22);
+            this.lblSelectUsuario.TabIndex = 0;
+            this.lblSelectUsuario.Text = "Seleccione el usuario a Modificar:";
+            // 
+            // txtUserModif
+            // 
+            this.txtUserModif.Location = new System.Drawing.Point(345, 98);
+            this.txtUserModif.Name = "txtUserModif";
+            this.txtUserModif.ReadOnly = true;
+            this.txtUserModif.Size = new System.Drawing.Size(276, 28);
+            this.txtUserModif.TabIndex = 18;
+            this.txtUserModif.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblIDUsuarioModi
+            // 
+            this.lblIDUsuarioModi.AutoSize = true;
+            this.lblIDUsuarioModi.Location = new System.Drawing.Point(15, 101);
+            this.lblIDUsuarioModi.Name = "lblIDUsuarioModi";
+            this.lblIDUsuarioModi.Size = new System.Drawing.Size(253, 22);
+            this.lblIDUsuarioModi.TabIndex = 17;
+            this.lblIDUsuarioModi.Text = "ID del usuario a Modificar:";
+            // 
+            // frmCambiarEstado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmCambiarEstado";
+            this.ClientSize = new System.Drawing.Size(674, 370);
+            this.Controls.Add(this.grpCambioDeEstado);
+            this.Name = "frmCambiarEstado";
+            this.Text = "Cambiar Estado";
+            this.Load += new System.EventHandler(this.frmCambiarEstado_Load);
+            this.grpCambioDeEstado.ResumeLayout(false);
+            this.grpCambioDeEstado.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpCambioDeEstado;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardarEstado;
+        private System.Windows.Forms.ComboBox cmbNuevoEstado;
+        private System.Windows.Forms.Label lblNUevoEstado;
+        private System.Windows.Forms.ComboBox cmblistaUsuarios;
+        private System.Windows.Forms.Label lblSelectUsuario;
+        private System.Windows.Forms.TextBox txtUserModif;
+        private System.Windows.Forms.Label lblIDUsuarioModi;
     }
 }
--- Interfaz/frmCambiarEstado.cs
+++ Interfaz/frmCambiarEstado.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Negocios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,102 @@ namespace Interfaz
         {
             InitializeComponent();
         }
-    }
-}
+        //VARIABLES GLOBALES
+         Negocios_MySQL objNegocios = new Negocios_MySQL();
+        DataTable dt_Usuarios = new DataTable();//tendra la info que viene desde la BD
+        private void frmCambiarEstado_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    dt_Usuarios = objNegocios.QueryMySQLNegocios_DT("Select * from usuario");
+
+                    //se recorre el DT con los usuarios para agregar el ususario al combo
+                    foreach (DataRow fila in dt_Usuarios.Rows)
+                    {
+                        //agrega un item por cada fila en la columna Usuario
+                        cmblistaUsuarios.Items.Add(fila["Usuario"].ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }//fin load
+
+        private void cmblistaUsuarios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataRow fila in dt_Usuarios.Rows)
+                {
+                    if (fila["Usuario"].ToString().Equals(cmblistaUsuarios.Text))
+                    {
+                        txtUserModif.Text = fila["Id"].ToString();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }//fin cmblistaUsuarios_SelectedIndexChanged
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuardarEstado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //VALIDACIONES
+                while (cmblistaUsuarios.Text == "")
+                {
+                    MessageBox.Show("Aun no seleciona el Usuario de la Lista");
+                    return;
+
+                }
+                while (cmbNuevoEstado.Text == "")
+                {
+                    MessageBox.Show("Aun no seleciona el Nuevo Estado");
+                    return;
+                }
+                //FIN VALIDACIONES
+
+                byte estadoBit = 0;
+                String UsuarioElegido = cmblistaUsuarios.Text;//guarda el usuario q se eligio
+
+                //si selecionaron activo mandamos un 1
+                if (cmbNuevoEstado.Text.Equals("Activo"))
+                {
+                    estadoBit = 1;
+                }
+                else
+                {
+                    estadoBit = 0;
+                }
+
+                objNegocios.QueryMySQLNegocios_DT(" UPDATE usuario SET Estado = " + estadoBit +
+                   "   WHERE Usuario = '" + UsuarioElegido + "';");
+
+                MessageBox.Show("El estado se modifico Correctamente");
+                this.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }//fin btnGuardarEstado_Click
+    }//fin class
+}//fin space
